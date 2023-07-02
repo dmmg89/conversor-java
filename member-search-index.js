e91158fe83a9c12e6f6fd8344a3ae4f0fd27b1cc
dmmg89 <dvmunozgarcia@gmail.com>
2023-07-01 22:50:23 -0600

    Se modificó la estructura principal, se agregaron 2 listas desplegables y botores de tipo ratio

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Converter","l":"Converter()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Converter","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ src/ConversionData/LengthEquivalencies.java
@@ -0,0 +1,16 @@
+package ConversionData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthEquivalencies {
+
+    private String[][] longitud = {
+            {"metro", "1.0"},
+            {"centimetro", "100"},
+            {"milimetro", "1000"},
+    };
+
+   // public static String getPair(String key){      longitud    };
+
+}
